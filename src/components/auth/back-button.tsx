f2d2e51b598ff1c782df0604f20f16178e032315
henrymiessan <miessanhenri@gmail.com>
2024-06-20 16:19:19 +0000

    work on form validation

---
+++ src/components/auth/back-button.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+interface BackButtonProps {
+  label: string;
+  href: string;
+}
+
+const BackButton = ({ label, href }: BackButtonProps) => {
+  return (
+    <div>
+      <Button variant="link" className="font-normal w-full" size="sm" asChild />
+      <Link href={href}>{label}</Link>
+    </div>
+  );
+};
+
+export default BackButton;
---
+++ src/components/auth/card-wrapper.tsx
@@ -0,0 +1,40 @@
+"use client ";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
+import AuthHeader from "./auth-header";
+import BackButton from "./back-button";
+
+interface CardWrapperProps {
+  label: string;
+  title: string;
+  backButtonHref: string;
+  backButtonLabel: string;
+  children: React.ReactNode;
+}
+
+const CardWrapper = ({
+  label,
+  title,
+  backButtonHref,
+  backButtonLabel,
+  children,
+}: CardWrapperProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <AuthHeader label={label} title={title} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CardWrapper;
